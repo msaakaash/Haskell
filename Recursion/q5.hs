d1a39d18e94772a048950da55e18af24c307557e
msaakaash <msaakaash@hotmail.com>
2025-02-07 18:18:47 +0530

    Updated

--- Recursion/q5.hs
+++ Recursion/q5.hs
@@ -1,11 +1,10 @@
 --sum of digits of a number
 
 sumOfDigits :: Int -> Int 
-sumOfDigits x
-    | x <= 0 = 0
-    | otherwise = (mod x 10) + (sumOfDigits (div x 10))
+sumOfDigits 0 = 0
+sumOfDigits x = (mod x 10) + (sumOfDigits (div x 10))
 
 
 main = do
-    let a = 123456789
+    let a = 12345
     putStrLn $ "The sum of digits in " ++ show a ++ " is:" ++ show(sumOfDigits a)
