01ee1e2924c78b14e629e556fb44b711dfc44090
Aakaash M S <msaakaash@hotmail.com>
2025-02-07 18:23:08 +0530

    Delete prime.hs

--- prime.hs
+++
@@ -1,9 +0,0 @@
-isPrime :: Int -> Bool
-isPrime n = null [d | d <- [2..n-1],n `mod` d == 0]
-
-main = do
-  putStr "Enter a number:"
-  x <- getLine
-  let n1 = read x :: Int
-  if isPrime n1 then print(show n1 ++ " is a prime number")
-  else print(show n1 ++ " is a not prime number")
